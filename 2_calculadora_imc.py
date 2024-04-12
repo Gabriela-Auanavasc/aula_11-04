e2f118c601fc16506852899dfce12235095f5668
Gabriela-Auanavasc <165970579+Gabriela-Auanavasc@users.noreply.github.com>
2024-04-11 21:03:23 -0400

    Update 2_calculadora_imc.py

--- 2_calculadora_imc.py
+++ 2_calculadora_imc.py
@@ -1,4 +1,11 @@
 
+# O código abaixo é uma tentativa de criar uma calculadora de Índice de Massa Corporal (IMC).
+# No entanto, ele está incompleto e contém erros.
+# Modifique o código para que ele solicite ao usuário seu peso em quilogramas e altura em metros.
+# Seu programa deve calcular e imprimir o IMC do usuário seguindo a fórmula IMC = peso / (altura ** 2).
+# Além disso, deve classificar o resultado em: Baixo peso, Peso normal, Sobrepeso ou Obesidade, 
+# baseando-se nos valores de IMC.
+
 peso = float(input('digite seu peso em kg> '))
 altura = float(input('digite sua altura em metros> '))
 
